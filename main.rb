4edacef4f9d9cd2d009a1c77a332ff8f7ea27e4c
Sharon L <sluong@spoonflower.com>
2014-04-22 21:58:41 -0400

    moving forward with initial game setup

--- main.rb
+++ main.rb
@@ -3,6 +3,31 @@ require 'sinatra'
 
 set :sessions, true
 
+helpers do
+
+	def calculate_total(cards)
+		arr = cards.map{|e| e[1]}
+
+		total = 0
+		arr.each do |a|
+			if a == 'A'
+				total += 11
+			elsif a.to_i == 0
+				total += 10
+			else
+				total += a.to_i
+			end
+		end
+		arr.select{|e| e == 'A'}.count.times do
+			if total > 21
+				total -=10
+			end
+		end
+		total
+	end
+
+end
+
 get '/' do
 	redirect '/new_game'
 end
@@ -11,3 +36,22 @@ get '/new_game' do
 	erb :new_game
 end
 
+post '/new_game' do
+	session[:player_name] = params[:player_name]
+	redirect '/game'
+end
+
+get '/game' do
+	suits = %w[diamonds clubs hearts spades]
+	cards = %w[A 2 3 4 5 6 7 8 9 10 J Q K]
+	session[:deck] = cards.product(suits)
+	session[:deck].shuffle!
+	session[:player_cards] = []
+	session[:dealer_cards] = []
+	session[:player_cards] << session[:deck].pop
+	session[:dealer_cards] << session[:deck].pop
+	session[:player_cards] << session[:deck].pop
+	session[:dealer_cards] << session[:deck].pop
+	
+	erb :game
+end
